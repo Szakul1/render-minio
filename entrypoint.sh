3988140536882fa2bdfaa82b58800b5d4d475ec5
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2022-12-02 20:19:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # wrapper for docker entrypoint that takes into account the PORT env var
 
-exec docker-entrypoint.sh server --console-address "0.0.0.0:10000" --address "0.0.0.0:9000" /data
+exec docker-entrypoint.sh server --console-address "0.0.0.0:10000" --address "0.0.0.0:9000" /data 
